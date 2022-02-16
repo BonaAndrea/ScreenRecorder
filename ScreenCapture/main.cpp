b8cb89db0041029725882bc8f7f7ad33f2878470
AndreaBona <andrea.bona1996@gmail.com>
2022-02-16 22:51:48 +0100

    Main commentato

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -6,28 +6,16 @@
 #endif
 using namespace std;
 
-
-/* driver function to run the application */
 int main(int argc, char* argv[])
 {
-
-#if QT
+	/*
+	* Crea QT Application e Widget
+	*/
 	QApplication a(argc, argv);
 	QtWidgetsClass w;
 	w.setWindowTitle(QString("Vola mio mini recorder"));
 
 	w.show();
 	a.exec();
-	
-#endif
-
-#if !QT
-	screen_record.openVideoDevice();
-	screen_record.openAudioDevice();
-
-	screen_record.initOutputFile();
-	screen_record.CreateThreads();
-#endif
-
 	return 0;
 }
